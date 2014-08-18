e54cb1b9816ef29bf9cf0ed4e5f61dd96aa3c9cf
Nick Weaver <jnweaver@wisc.edu>
2014-08-18 16:15:15 -0500

    Can we run two actions in the same block?

--- metadata.rb
+++ metadata.rb
@@ -4,6 +4,6 @@ maintainer_email 'jnweaver@wisc.edu'
 license          'All rights reserved'
 description      'Install and configures tools for building UW-Madison Wordpress sites'
 long_description 'TBD'
-version          '0.1.10'
+version          '0.1.11'
 
 depends "grunt_cookbook"

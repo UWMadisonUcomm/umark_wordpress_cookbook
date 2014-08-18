2509d16d241cd874d5387125a70845a05a7d3a15
Nick Weaver <jnweaver@wisc.edu>
2014-08-17 21:37:57 -0500

    Ad provider for bower resource

---
+++ providers/bower.rb
@@ -0,0 +1,36 @@
+#
+# Cookbook: uw_wordpress
+# uw_wordpress_bower
+#
+
+action :install do
+  # Import
+  dir = @new_resource.dir
+  package = @new_resource.package
+  flags = @new_resource.flags
+
+  # Check
+  unless dir then
+    raise "No directory supplied, should be defined using the name parameter."
+  end
+
+  unless ::File.directory?(dir) then
+    raise "Directory '#{dir}' to run bower action from was not found"
+  end
+
+  # Prepare NPM command
+  bower_command = "bower install"
+
+  unless package.nil? or package.empty?
+    bower_command += " #{package}"
+  end
+
+  unless flags.nil? or flags.empty?
+      bower_command += " #{flags}"
+  end
+
+  execute bower_command do
+    cwd dir
+  end
+
+end

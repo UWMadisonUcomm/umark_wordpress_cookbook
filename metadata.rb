0d2930583a63bc6282049a75f84ec6d42d223a30
Nick Weaver <jnweaver@wisc.edu>
2014-08-26 15:39:46 -0500

    Rename the cookbook

--- metadata.rb
+++ metadata.rb
@@ -1,15 +1,14 @@
-name             'uw_wordpress'
+name             'umark_wordpress'
 maintainer       'Nick Weaver'
 maintainer_email 'jnweaver@wisc.edu'
 license          'All rights reserved'
 description      'Install and configure tools for building UW-Madison Wordpress sites'
-version          '0.2.10'
+version          '0.3.0'
 
 depends "grunt_cookbook"
 depends "git"
 depends "subversion"
 depends "database"
-depends "mysql"
 depends "composer"
 depends "nodejs"
 depends 'magic_shell'

feda0a897c3e5286015d129c2d4d68f2bdb5bce0
Nick Weaver <jnweaver@wisc.edu>
2014-08-26 12:01:35 -0500

    Add mysql?

--- metadata.rb
+++ metadata.rb
@@ -3,12 +3,13 @@ maintainer       'Nick Weaver'
 maintainer_email 'jnweaver@wisc.edu'
 license          'All rights reserved'
 description      'Install and configure tools for building UW-Madison Wordpress sites'
-version          '0.2.9'
+version          '0.2.10'
 
 depends "grunt_cookbook"
 depends "git"
 depends "subversion"
 depends "database"
+depends "mysql"
 depends "composer"
 depends "nodejs"
 depends 'magic_shell'
